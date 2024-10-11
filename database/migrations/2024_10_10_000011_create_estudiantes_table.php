b67508c7c65c7578333280b54d6774ff64b69933
ImJesusGomez <jesusgomez.dev10@gmail.com>
2024-10-10 20:48:28 -0600

    Solución del error en la base de datos

--- database/migrations/2024_10_10_000011_create_estudiantes_table.php
+++ database/migrations/2024_10_10_000011_create_estudiantes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('estudiantes_table', function (Blueprint $table) {
+        Schema::create('estudiantes', function (Blueprint $table) {
             $table->id();
 
             $table->string('nombre');

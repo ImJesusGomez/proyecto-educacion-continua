d50502b3e1ca8bd7aca7ac697add1464bcc1cf9d
DennisRogelio <drrm77485@gmail.com>
2024-10-10 18:03:59 -0600

    Creacion del modelo Constancia y migraciones constancias y estudiantes_cursos

---
+++ app/Models/constancia.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Constancia extends Model
+{
+    use HasFactory;
+
+    public function estudiante(){
+        return $this->belongsTo(Estudiante::class);
+    }
+
+    public function curso(){
+        return $this->belongsTo(Curso::class);
+    }
+
+    public function docente(){
+        return $this->belongsTo(Docente::class);
+    }
+}
---
+++ database/migrations/2024_10_10_225539_create_constancias_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('constancias', function (Blueprint $table) {
+            $table->id();
+            $table->float('calificacion');
+            $table->date('fecha');
+            $table->foreignId('estudiante_id')->constrained('estudiantes','id')->onDelete('cascade');
+            $table->foreignId('curso_id')->constrained('cursos','id')->onDelete('cascade');
+            $table->foreignId('docente_id')->constrained('docentes','id')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('constancias');
+    }
+};
